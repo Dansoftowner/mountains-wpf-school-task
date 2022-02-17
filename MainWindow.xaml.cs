795a9e9d36e4f67d695867cfca2fb5aa13df5b95
Daniel Gyoerffy <49782833+Dansoftowner@users.noreply.github.com>
2022-02-17 17:04:15 +0100

    finished task

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,79 +23,122 @@ namespace Györffy_Dániel_Hegyek_WPF_11A
     {
 
         private const string FilePath = "hegyekMo.txt";
-        private const string Separator = ";";
 
-        private readonly List<Mountain> mountains;
+        private const double CanvasLeft = 10;
+        private const double CanvasTop = 5;
+        private const double Spacing = 40;
+
+        private readonly Mountains Mountains = new Mountains(FilePath);
 
         public MainWindow()
         {
             InitializeComponent();
-            mountains = InitializeFileContent();
             ExecuteTasks();
         }
 
-        private List<Mountain> InitializeFileContent() =>
-            File.ReadAllLines(FilePath)
-            .Skip(1)
-            .Select(it => it.Split(Separator))
-            .Select(it => new Mountain(it[0], it[1], int.Parse(it[2])))
-            .ToList();
-        
         private void ExecuteTasks()
         {
             TaskOne();
             TaskTwo();
             TaskThree();
             TaskFour();
+            TaskFive();
         }
 
         private void TaskOne()
         {
-            WriteSimpleMessage(1, $"Az állományban szereplő hegycsúcsok száma: {mountains.Count}");
+            var label = BuildTaskLabel(1, $"Az állományban szereplő hegycsúcsok száma: {Mountains.MountainsCount}");
+            RootCanvas.Children.Add(label);
         }
 
         private void TaskTwo()
         {
-            double averageHeight = mountains.Select(it => it.Height).Average();
-            WriteSimpleMessage(2, $"A hegycsúcsok átlag magassága: {averageHeight} méter");
+            var label = BuildTaskLabel(2, $"A hegycsúcsok átlag magassága: {Mountains.AverageHeight} méter");
+            RootCanvas.Children.Add(label);
         }
 
         private void TaskThree()
         {
-            Mountain highest = mountains.Aggregate((left, right) => left.Height > right.Height ? left : right);
-            WriteSimpleMessage(3, $"A legmagasabb hegy: {highest.MountainTeritory}, magassága: {highest.Height}");
+            Mountain highest = Mountains.HighestMountain;
+            var label = BuildTaskLabel(3, $"A legmagasabb hegy: {highest.MountainTeritory}, magassága: {highest.Height}");
+            RootCanvas.Children.Add(label);
         }
 
         private void TaskFour()
         {
+            var taskLabel = BuildTaskLabel(4);
+
             ComboBox combo = new ComboBox();
             combo.Width = 150;
             combo.Height = 30;
-            combo.ItemsSource = mountains.Select(it => it.MountainTeritory).Distinct();
+            combo.ItemsSource = Mountains.DistinctMountainTerritories;
+            Canvas.SetLeft(combo, taskLabel.ActualWidth + CanvasLeft + 80);
+            Canvas.SetTop(combo, Canvas.GetTop(taskLabel));
+
+            var labelTwo = BuildSimpleLabel("hegység hegycsúcsai:");
+            Canvas.SetLeft(labelTwo, CanvasLeft + taskLabel.ActualWidth + combo.ActualWidth + 250);
+            Canvas.SetTop(labelTwo, Canvas.GetTop(taskLabel));
 
             ListView listView = new ListView();
             listView.Width = 150;
+            listView.Height = 130;
             combo.SelectionChanged += new SelectionChangedEventHandler((s, args) => {
-                listView.ItemsSource = mountains.Where(it => it.MountainTeritory.Equals(combo.SelectedItem)).Select(it => it.Name);
+                listView.ItemsSource = Mountains.GetByTerritory((string)combo.SelectedItem).Select(it => it.Name);
             });
+            Canvas.SetLeft(listView, CanvasLeft + taskLabel.ActualWidth + combo.ActualWidth + labelTwo.ActualWidth + 400);
+            Canvas.SetTop(listView, Canvas.GetTop(taskLabel));
 
-            AddRow(GridLength.Auto, BuildTaskLabel(4), combo, BuildSimpleLabel("hegység hegycsúcsai:"), listView);
+            RootCanvas.Children.Add(taskLabel);
+            RootCanvas.Children.Add(combo);
+            RootCanvas.Children.Add(labelTwo);
+            RootCanvas.Children.Add(listView);
         }
 
         private void TaskFive()
         {
+            var taskLabel = BuildTaskLabel(5);
+            Canvas.SetTop(taskLabel, Canvas.GetTop(taskLabel) + 100);
+
+            var labelTwo = BuildSimpleLabel("méter feletti hegycsúcsok száma:");
+            Canvas.SetTop(labelTwo, Canvas.GetTop(taskLabel));
+            Canvas.SetLeft(labelTwo, CanvasLeft + 200);
+
+            var labelThree = new Label();
+            Canvas.SetTop(labelThree, Canvas.GetTop(taskLabel));
+            Canvas.SetLeft(labelThree, CanvasLeft + 400);
+
             TextBox input = new TextBox();
+            input.Width = 50;
+            input.Height = 30;
+            input.TextChanged += new TextChangedEventHandler((s, args) => {
+                try
+                {
+                    labelThree.Content = Mountains.GetHigherCount(int.Parse(input.Text));
+                } 
+                catch(FormatException)
+                {
+                    labelThree.Content = "Nincs adat";
+                }
+            });
+            Canvas.SetLeft(input, CanvasLeft + taskLabel.ActualWidth + 100);
+            Canvas.SetTop(input, Canvas.GetTop(taskLabel));
+            
+
+            RootCanvas.Children.Add(taskLabel);
+            RootCanvas.Children.Add(input);
+            RootCanvas.Children.Add(labelTwo);
+            RootCanvas.Children.Add(labelThree);
+
         }
 
-        private void WriteSimpleMessage(int n, string content)
-        {
-            AddRow(BuildTaskLabel(n, content), new GridLength(40));
-        }
-
+    
         private Label BuildTaskLabel(int n, string content = "")
         {
             var label = BuildSimpleLabel($"{n}. feladat: {content}");
             label.FontSize = 15;
+
+            Canvas.SetLeft(label, CanvasLeft);
+            Canvas.SetTop(label, CanvasTop + n * Spacing);
             return label;
         }
 
@@ -106,35 +149,5 @@ namespace Györffy_Dániel_Hegyek_WPF_11A
             return label;
         }
 
-        private void AddRow(Control control, GridLength height)
-        {
-            control.SetValue(Grid.RowProperty, RootGrid.RowDefinitions.Count());
-
-            var rowDefiniton = new RowDefinition();
-            rowDefiniton.Height = height;
-
-            RootGrid.RowDefinitions.Add(rowDefiniton);
-            RootGrid.Children.Add(control);
-        }
-
-        private void AddRow(GridLength height, params Control[] controls)
-        {
-            int row = RootGrid.RowDefinitions.Count();
-            var rowDefiniton = new RowDefinition();
-            rowDefiniton.Height = height;
-            RootGrid.RowDefinitions.Add(rowDefiniton);
-
-            for (int i = 0; i < controls.Length; i++)
-            {
-                var colDefiniton = new ColumnDefinition();
-                colDefiniton.Width = new GridLength(150);
-
-                controls[i].SetValue(Grid.ColumnProperty, i);
-                controls[i].SetValue(Grid.RowProperty, row);
-                RootGrid.ColumnDefinitions.Add(colDefiniton);
-                RootGrid.Children.Add(controls[i]);
-            }
-
-        }
     }
 }
--- Mountains.cs
+++ Mountains.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Györffy_Dániel_Hegyek_WPF_11A
 {
     class Mountains
     {
+
+        private List<Mountain> MountainsList;
+
+        public int MountainsCount => MountainsList.Count;
+        public double AverageHeight => MountainsList.Select(it => it.Height).Average();
+        public Mountain HighestMountain => MountainsList.Aggregate((left, right) => left.Height > right.Height ? left : right);
+        public List<string> DistinctMountainTerritories => MountainsList.Select(it => it.MountainTeritory).Distinct().ToList();
+
+        public Mountains(List<Mountain> mountainsList)
+        {
+            MountainsList = mountainsList;
+        }
+
+        public Mountains(string filePath): this(
+            File.ReadAllLines(filePath)
+            .Skip(1)
+            .Select(it => it.Split(";"))
+            .Select(it => new Mountain(it[0], it[1], int.Parse(it[2])))
+            .ToList()
+        ) { }
+
+        public List<Mountain> GetByTerritory(string territory) =>
+            MountainsList.Where(it => it.MountainTeritory.Equals(territory)).ToList();
+
+        public int GetHigherCount(int height) =>
+            MountainsList.Where(it => it.Height > height).Count();
+
     }
 }
